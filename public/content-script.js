2652c239be602c5c1a8810326440c8b7edd8199a
topdev7 <gabriel.arias.sh@gmail.com>
2023-04-04 12:38:42 -0300

    update param

--- public/content-script.js
+++ public/content-script.js
@@ -20,7 +20,7 @@ window.onload = async (event) => {
   }
 
   patientInfo = await fetch(
-    "https://shazzle-api.herokuapp.com/api/patients/289"
+    "https://shazzle-api.herokuapp.com/api/patients/353"
   )
     .then((response) => response.json())
     .then((res) => res.data.patient);
